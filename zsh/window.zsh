94973be43f8678797d840d37e024ea41706d979f
Andy Blyler <andy@blyler.cc>
2020-02-29 10:36:28 -0500

    Add ability to toggle window / tab title auto updating

--- zsh/window.zsh
+++ zsh/window.zsh
@@ -1,3 +1,46 @@
+# copied from pure; modified to honor disable title updating
+prompt_pure_set_title() {
+  setopt localoptions noshwordsplit
+
+  # Honor the disabled auto title flag
+  [[ "$DISABLE_AUTO_TITLE" == "true" ]] && return
+
+  # Emacs terminal does not support settings the title.
+  (( ${+EMACS} || ${+INSIDE_EMACS} )) && return
+
+  case $TTY in
+    # Don't set title over serial console.
+    /dev/ttyS[0-9]*) return;;
+  esac
+
+  # Show hostname if connected via SSH.
+  local hostname=
+  if [[ -n $prompt_pure_state[username] ]]; then
+    # Expand in-place in case ignore-escape is used.
+    hostname="${(%):-(%m) }"
+  fi
+
+  local -a opts
+  case $1 in
+    expand-prompt) opts=(-P);;
+    ignore-escape) opts=(-r);;
+  esac
+
+  # Set title atomically in one print statement so that it works when XTRACE is enabled.
+  print -n $opts $'\e]0;'${hostname}${2}$'\a'
+}
+
+# disable auto updating title
+auto_title_disable() {
+  export DISABLE_AUTO_TITLE="true"
+  title "$PWD"
+}
+
+# enable auto updating title
+auto_title_enable() {
+  export DISABLE_AUTO_TITLE="false"
+}
+
 # From http://dotfiles.org/~_why/.zshrc and https://github.com/robbyrussell/oh-my-zsh/blob/master/lib/termsupport.zsh
 # for my purposes, i don't care about window title, just the tab title given to tmux,
 function title() {
